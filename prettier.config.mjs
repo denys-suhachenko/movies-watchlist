ede3bb57c20f69070f873ce727fcaae669af6256
Denys <denys.x95@gmail.com>
2026-04-09 09:07:56 +0300

    added prettier and format code

---
+++ prettier.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const prettierConfig = {
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default prettierConfig;
